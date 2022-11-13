481da87fd6f49d24daa62e4501fff61c9d686185
buseilguz <buse.ilguz@gmail.com>
2022-11-13 18:38:19 +0300

    SayiTahmini

---
+++ OgrenciBilgiSistemi/src/SayiTahmin/Main.java
@@ -0,0 +1,61 @@
+package SayiTahmin;
+
+import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Random rnd = new Random();
+        int number = rnd.nextInt(100);
+       
+        Scanner scn = new Scanner(System.in);
+        int right = 0;
+        int selected;
+        int[] wrong = new int[10];
+        boolean isWin = false;
+        boolean isWrong = false;
+
+        System.out.println(number);
+        while (right < 10) {
+            System.out.print("Lütfen tahmininizi giriniz : ");
+            selected = scn.nextInt();
+
+            if (selected < 0 || selected > 99) {
+                System.out.println("Lütfen 0-100 arasýnda bir deðer giriniz.");
+                if (isWrong) {
+                    right++;
+                    System.out.println("Çok fazla hatalý giriþ yaptýnýz. Kalan hak : " + (10 - right));
+                } else {
+                    isWrong = true;
+                    System.out.println("Bir daha hatalý giriþinizde hakkýnýzdan düþülecektir.");
+                }
+                continue;
+            }
+
+            if (selected == number) {
+                System.out.println("Tebrikler, doðru tahmin ! Tahmin ettiðini sayý : " + number);
+                isWin = true;
+                break;
+            } else {
+                System.out.println("Hatalý bir sayý girdiniz !");
+                if (selected > number) {
+                    System.out.println(selected + " sayýsý, gizli sayýdan büyüktür.");
+                } else {
+                    System.out.println(selected + " sayýsý, gizli sayýdan küçüktür.");
+                }
+
+                wrong[right++] = selected;
+                System.out.println("Kalan tahmin hakký : " + (10 - right));
+            }
+        }
+
+        if (!isWin) {
+            System.out.println("Kaybettiniz ! ");
+            if (!isWrong) {
+                System.out.println("Tahminleriniz : " + Arrays.toString(wrong));
+            }
+        }
+
+    }
+}
